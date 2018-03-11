dd5189faa2df0615308d212eef9e5b2c828ce3f2
Wei Ren <weiren@berkeley.edu>
2018-03-11 09:42:21 -0700

    test

--- api_db.py
+++ api_db.py
@@ -10,28 +10,28 @@ import logging
 import argparse
 import random
 import os
-# app = Flask(__name__)
-# CORS(app)
+app = Flask(__name__)
+CORS(app)
 
-# @app.route("/webclient", methods=["GET"])
-# def get_all_webclient():
-#     response = table.scan()
-#     items = response['Items']
-#     return_data = list()
-#     timeNow = datetime.datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S'), "%Y-%m-%d %H:%M:%S")
-#     for item in items:
-#         data_dict = dict()
-#         data_dict["clientId"] = item["Id"]
-#         data_dict["Time"] = item["Time"]
-#         if ((datetime.datetime.strptime(item["LastTime"], "%Y-%m-%d %H:%M:%S") - timeNow).total_seconds() > 5):
-#             ata_dict["WebStatus"] = 'offline'
-#         else:
-#             data_dict["WebStatus"] = item["WebStatus"]
-#         data_dict["User"] = item["User"]
-#         data_dict["LastTime"] = item["LastTime"]
-#         return_data.append(data_dict)
-#     result = sorted(return_data, key=lambda k: k['Time'], reverse=True)
-#     return json.dumps(result)
+@app.route("/webclient", methods=["GET"])
+def get_all_webclient():
+    response = table.scan()
+    items = response['Items']
+    return_data = list()
+    timeNow = datetime.datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S'), "%Y-%m-%d %H:%M:%S")
+    for item in items:
+        data_dict = dict()
+        data_dict["clientId"] = item["Id"]
+        data_dict["Time"] = item["Time"]
+        if ((datetime.datetime.strptime(item["LastTime"], "%Y-%m-%d %H:%M:%S") - timeNow).total_seconds() > 5):
+            ata_dict["WebStatus"] = 'offline'
+        else:
+            data_dict["WebStatus"] = item["WebStatus"]
+        data_dict["User"] = item["User"]
+        data_dict["LastTime"] = item["LastTime"]
+        return_data.append(data_dict)
+    result = sorted(return_data, key=lambda k: k['Time'], reverse=True)
+    return json.dumps(result)
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
     # print("Received a new message " + "from topic: " + message.topic)
@@ -147,8 +147,8 @@ myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 myAWSIoTMQTTClient.connect()
 myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
 time.sleep(2)
-while(1):
-    time.sleep(2)
-# if __name__ == "__main__":
-#     app.debug = False
-#     app.run(host='0.0.0.0', port=8080)
+# while(1):
+#     time.sleep(2)
+if __name__ == "__main__":
+    app.debug = False
+    app.run(host='0.0.0.0', port=80)
