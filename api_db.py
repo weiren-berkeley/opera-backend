504c8d59c5b8e7bfebfb911bdefae7b6f6a229a1
Wei Ren <weiren@berkeley.edu>
2018-03-11 09:46:41 -0700

    devide

--- api_db.py
+++ api_db.py
@@ -32,46 +32,7 @@ def get_all_webclient():
         return_data.append(data_dict)
     result = sorted(return_data, key=lambda k: k['Time'], reverse=True)
     return json.dumps(result)
-# Custom MQTT message callback
-def customCallback(client, userdata, message):
-    # print("Received a new message " + "from topic: " + message.topic)
-    obj = json.loads(message.payload)
-    print(message.payload)
-    # print(obj['text'])
-    if (obj['type'] == 'status' and 'Id'in obj):
-        # print(obj['status'])
-        # print(obj['Id'])
-        a = datetime.datetime.strptime('2018-03-11 06:00:00', "%Y-%m-%d %H:%M:%S")
-        b = datetime.datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S'), "%Y-%m-%d %H:%M:%S")
-        # print((b-a).total_seconds())
-        response = table.query(
-            KeyConditionExpression=Key('Id').eq(obj['Id'])
-        )
-        items = response['Items']
-        if (items == []):
-            table.put_item(
-               Item={
-                    'Id': obj['Id'],
-                    'Time': time.strftime('%Y-%m-%d %H:%M:%S'),
-                    'WebStatus': obj['WebStatus'],
-                    'User': obj['Id'],
-                    'LastTime': time.strftime('%Y-%m-%d %H:%M:%S')
-                }
-            )
-        else:
-            table.update_item(
-                Key={
-                    'Id': obj['Id'],
-                    'Time': items[0]['Time']
-                },
-                UpdateExpression='SET WebStatus = :val1, LastTime = :val2',
-                ExpressionAttributeValues={
-                    ':val1': obj['WebStatus'],
-                    ':val2': time.strftime('%Y-%m-%d %H:%M:%S')
-                }
-            )
-# print("--------------")
-# IoT Seetings
+
 
 
 # Get the service resource.
@@ -120,33 +81,6 @@ table = dynamodb.Table('OPARP-Client')
 #     }
 # )
 
-host = 'a21zozqgendyv9.iot.us-east-2.amazonaws.com'
-rootCAPath = 'root-CA.crt'
-certificatePath = 'certificate.pem.crt'
-privateKeyPath = 'private.pem.key'
-useWebsocket = False
-clientId = 'Server' + str(int(random.random()*10000000000000))
-print('clintId: ' + clientId)
-topic = 'oparp'
-mode = 'both'
-
-# Init AWSIoTMQTTClient
-myAWSIoTMQTTClient = None
-myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-myAWSIoTMQTTClient.configureEndpoint(host, 8883)
-myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
-
-# AWSIoTMQTTClient connection configuration
-myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-
-# Connect and subscribe to AWS IoT
-myAWSIoTMQTTClient.connect()
-myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
-time.sleep(2)
 # while(1):
 #     time.sleep(2)
 if __name__ == "__main__":
