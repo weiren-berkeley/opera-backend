729fbf89a0048d8ef6bf801bd26824c4e637e477
Wei Ren <weiren@berkeley.edu>
2018-03-20 05:20:40 -0700

    add publish

--- api_db.py
+++ api_db.py
@@ -3,7 +3,7 @@ from boto3.dynamodb.conditions import Key, Attr
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import time
 import datetime
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, json
 import json
 from flask_cors import CORS
 import logging
@@ -12,6 +12,63 @@ import random
 import os
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/publish", methods=["POST"])
+def publish_programming():
+    clientID = request.form["clientID"]
+    data = request.form["data"]
+    projectName = request.form["projectName"]
+    emailAdress = request.form["emailAdress"]
+    yourName = request.form["yourName"]
+    description = request.form["description"]
+    print(projectName)
+    tablePublish.put_item(
+       Item={
+            'clientID': clientID,
+            'time': time.strftime('%Y-%m-%d %H:%M:%S'),
+            'yourName': yourName,
+            'emailAdress': emailAdress,
+            'projectName': projectName,
+            'description': description,
+            'data': data
+        }
+    )
+    response = app.response_class(
+        json.dumps({
+        'status': 200,
+        'text': 'success',
+        }),
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/published", methods=["GET"])
+def get_all_published():
+    response = tablePublish.scan()
+    items = response['Items']
+    return_data = list()
+    for item in items:
+        data_dict = dict()
+        data_dict["clientID"] = item["clientID"]
+        data_dict["time"] = item["time"]
+        data_dict["yourName"] = item["yourName"]
+        data_dict["emailAdress"] = item["emailAdress"]
+        data_dict["projectName"] = item["projectName"]
+        data_dict["description"] = item["description"]
+        data_dict["data"] = item["data"]
+        return_data.append(data_dict)
+    result = sorted(return_data, key=lambda k: k['time'], reverse=True)
+    return json.dumps(result)
+
+@app.route("/onePublished", methods=["GET"])
+def get_one_published():
+    request_data = request.args
+    clientID = request_data['clientID']
+    response = tablePublish.query(
+        KeyConditionExpression=Key('clientID').eq(clientID)
+    )
+    items = response['Items']
+    return json.dumps(items[0])
 
 @app.route("/webclient", methods=["GET"])
 def get_all_webclient():
@@ -34,12 +91,12 @@ def get_all_webclient():
     return json.dumps(result)
 
 
-
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 # Print out some data about the table.
 table = dynamodb.Table('OPARP-Client')
+tablePublish = dynamodb.Table('OPARP-publish')
 
 if __name__ == "__main__":
-    app.debug = False
+    app.debug = True
     app.run(host='0.0.0.0', port=80)
