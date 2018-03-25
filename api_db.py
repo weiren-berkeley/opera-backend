135aa8f7245517f1f7160cc60048e8000a9dcda2
Wei Ren <weiren@berkeley.edu>
2018-03-24 19:54:43 -0700

    post

--- api_db.py
+++ api_db.py
@@ -18,7 +18,7 @@ CORS(app)
 def iotWrite():
     # request_data = request.get_json()
     request.get_json()
-    Device1ID = request_data["Device1ID"]
+    # Device1ID = request_data["Device1ID"]
     # Device1ID = request.args.get('Device1ID')
     # Device1ID = request.form['Device1ID']
     # Device1ID = request_data["Device1ID"]
