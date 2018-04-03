f3a3c1dbf863ae9da942c604c2f46c1bf364fa34
Wei Ren <weiren@berkeley.edu>
2018-04-03 16:25:57 -0700

    iot

--- api_db.py
+++ api_db.py
@@ -24,7 +24,7 @@ def iotGet():
         data_dict = dict()
         # data_dict["Device1ID"] = item["Device1ID"]
         # data_dict["Device1ID"] = item["Device1ID"]
-        data_dict["airTemp"] = item["airTemp"]
+        # data_dict["airTemp"] = item["airTemp"]
         # data_dict["airHumi"] = item["airHumi"]
         # data_dict["Device2ID"] = item["Device2ID"]
         # data_dict["temp2"] = item["temp2"]
